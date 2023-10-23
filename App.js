48325a4c45d11cd310ba3d52b38e45cbefc8921f
quangthao2002 <quangthao22122002@gmail.com>
2023-10-24 02:01:55 +0700

    doneTuan6

--- App.js
+++ App.js
@@ -1,128 +1,200 @@
-import { StatusBar } from "expo-status-bar";
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Image,TouchableHighlight } from "react-native";
-import Stars from "react-native-stars";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useState } from "react";
+import { Pressable } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
-
-export default function App() {
-  const [count, setCount] = useState(0);
-  const onPress = () => setCount(count + 1);
+const Home = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri: require("./assets/vs_blue.png"),
-        }}
-        style={styles.image}
-      />
-      <View>
-        <Text style={styles.namephone}>
+    <View style={style.container}>
+      <View style={style.img}>
+        <Image source={require("./assets/vs_blue.png")} style={style.image} />
+      </View>
+      <View style={style.dscContainer}>
+        <Text style={{ marginTop: 40, fontSize: 15, fontFamily: "Roboto" }}>
           Điện Thoại Vsmart Joy 3 - Hàng chính hãng
         </Text>
-      </View>
-      {/* <StatusBar style="auto" /> */}
-      <View style={styles.review}>
-        <Stars
-          default={2.5}
-          count={5}
-          half={true}
-          starSize={50}
-          fullStar={<Icon name={"star"} style={[styles.myStarStyle]} />}
-          emptyStar={<Icon name={"star-outline"} style={styles.myStarStyle} />}
-          halfStar={<Icon name={"star-half"} style={[styles.myStarStyle]} />}
-        />
-        <Text style={{ marginLeft: 40 }}>(Xem 828 đánh giá)</Text>
-      </View>
-      <View style={styles.price}>
-        <Text style={{ fontSize: 20, fontWeight: "bold" }}>1.990.000đ</Text>
-        <Text style={{ textDecorationLine: "line-through" ,marginLeft:125}}>2.990.000đ</Text>
-      </View>
-      <View style={styles.textFooter}>
-        <Text style={{color:"red",fontWeight:"900"}}>Ở ĐÂU RẺ HƠN HOÀN TIỀN</Text>
-        <Image source={{uri:require("./assets/question.png")}}  style={{width:20,height:20,marginLeft:15}}/>
-      </View>
-      
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button}>
-          <Text style={{fontWeight:"600"}}>4 MÀU-CHỌN MÀU</Text>
+        <View style={{ flexDirection: "row", marginTop: 15 }}>
+          <Image
+            source={require("./assets/star.png")}
+            style={{ width: 20, height: 20 }}
+          />
+          <Image
+            source={require("./assets/star.png")}
+            style={{ width: 20, height: 20 }}
+          />
+          <Image
+            source={require("./assets/star.png")}
+            style={{ width: 20, height: 20 }}
+          />
+          <Image
+            source={require("./assets/star.png")}
+            style={{ width: 20, height: 20 }}
+          />
+          <Image
+            source={require("./assets/star.png")}
+            style={{ width: 20, height: 20 }}
+          />
+          <Text style={{ marginLeft: 50 }}>(Xem 828 đánh giá)</Text>
         </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.buy}>
-          <Text style={{fontWeight:"bold" ,color:"white"}}>CHON MUA</Text>
+        <View style={{ marginTop: 15, flexDirection: "row" }}>
+          <Text
+            style={{ fontSize: 20, fontFamily: "Roboto", fontWeight: "bold" }}
+          >
+            2.290.000 đ
+          </Text>
+          <Text style={{ marginLeft: 30, textDecorationLine: "line-through" }}>
+            2.990.000 đ
+          </Text>
         </View>
-      </TouchableHighlight>
-      
-      
+        <Text style={{ color: "red", fontWeight: "bold", marginTop: 10 }}>
+          Ở ĐÂU RẺ HƠN HOÀN TIỀN
+        </Text>
+        <View
+          style={{
+            borderWidth: 1,
+            width: 300,
+            height: 40,
+            borderRadius: 10,
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: 10,
+          }}
+        >
+          <Pressable
+            onPress={() => {
+              console.log("aaahh");
+            }}
+          >
+            4 MÀU CHỌN 1 MÀU
+          </Pressable>
+        </View>
+        <View
+          style={{
+            borderWidth: 1,
+            width: 300,
+            height: 40,
+            borderRadius: 10,
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: 30,
+            backgroundColor: "red",
+          }}
+        >
+          <Pressable
+            onPress={() => {
+              navigation.navigate("ProductDetail");
+            }}
+          >
+            CHỌN MUA
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
+};
+const ProductDetail = ({ navigation }) => {
+  const [image, setImage] = useState(require("./assets/vs_blue.png"));
+  return (
+    <View>
+      <Pressable
+        onPress={() => {
+          navigation.goBack();
+        }}
+      >
+        <Text>Go back</Text>
+      </Pressable>
+      <View style={style2.container}>
+        <View style={style2.img}>
+          <Image
+            source={image}
+            style={{ width: 120, height: 170, resizeMode: "contain" }}
+          />
+          {/* <Text style={{}}>Điện Thoại Vsmart Joy 3 Hàng chính hãng</Text> */}
+        </View>
+        <View style={style2.box}>
+          <Pressable
+            onPress={() => {
+              setImage(require("./assets/vs_silver.png"));
+            }} style={style2.boxSliver}
+          ></Pressable>
+          <Pressable style={style2.boxRed} onPress={()=>{
+            setImage(require("./assets/vs_red.png"))
+          }}></Pressable>
+          <Pressable style={style2.boxBlack} onPress={()=>{
+            setImage(require("./assets/vs_black.png"))
+          }}></Pressable>
+          <Pressable style={style2.boxBlue} onPress={()=>{
+            setImage(require("./assets/vs_blue.png"))
+          }}></Pressable>
+        </View>
+      </View>
+    </View>
+  );
+};
+const style2 = StyleSheet.create({
+  box: {
+    alignItems: "center",
+    backgroundColor: "gray",
+  },
+  container: {},
+  boxSliver: {
+    marginTop: 10,
+    width: 100,
+    height: 100,
+    backgroundColor: "silver",
+    marginBottom: 10,
+  },
+  boxRed: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+    marginBottom: 10,
+  },
+  boxBlack: {
+    width: 100,
+    height: 100,
+    backgroundColor: "black",
+    marginBottom: 10,
+  },
+  boxBlue: {
+    width: 100,
+    height: 100,
+    backgroundColor: "blue",
+    marginBottom: 10,
+  },
+});
+const style = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  img: {
+    flex: 4,
     alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
   },
   image: {
-    width: 200,
-    height: 250,
-    resizeMode: "cover",
+    width: 250,
+    height: 350,
+    resizeMode: "contain",
   },
-  namephone: {
-    fontSize: 16,
-    // fontWeight: "bold",
+  dscContainer: {
+    flex: 6,
+    marginLeft: 50,
   },
-  myStarStyle: {
-    color: "yellow",
-    backgroundColor: "transparent",
-    textShadowColor: "black",
-    textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 2,
-    fontSize: 30,
-  },
-  review: {
-    marginTop: 15,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  price: {
-    marginTop: 10,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    spaceAround: 10,
-  },
-  textFooter: {
-    marginTop: 15,
-    flexDirection: "row",
-    left: 0,
-  },
-  button: {
-    marginTop: 15,
-    alignItems: 'center',
-    width: 326,
-    height: 50,
-    borderWidth:2,
-    borderRadius:10,
-    borderColor:"gray",
-    padding: 10,
-  },
-  buy: {
-    marginTop: 100,
-      backgroundColor:"red",
-      borderColor:"gray",
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius:10,
-      width:326,
-      height:50,
-      borderWidth:2,
-      marginBottom:0,
-    },
-  mgnLeft:{
-    marginLeft:40,
-  }
 });
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="ProductDetail" component={ProductDetail} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
